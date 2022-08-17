941e407ff8d02c59a9b44783995eed3c39d45636
MustafaPolatov <mustafapolatov18@gmail.com>
2022-08-17 19:22:34 +0500

    Tugadi

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 
 let data;
 
+let korzinka = [
+
+]
+
 function API() {
     $.ajax({
-        url:"",
+        url:"https://api.nytimes.com/svc/books/v3/lists/current/childrens-middle-grade.json?api-key=0nG5do2caU59G7F2PT1eRQD0RAsaX5Du",
         success: (keldi) =>{
-            console.log(keldi);
-            data = keldi
+            console.log(keldi.results.books);
+            data = keldi.results.books
+            Ekran(data)
         },
         error: (err) =>{
             console.log(err);
@@ -15,3 +20,84 @@ function API() {
 }
 
 API()
+
+
+
+
+    function Ekran(masiv) {
+        $("#map-card").html("")
+        masiv.map((v,i)=>{
+           let col = `
+            <div class="col-3">
+                <div class="map-card">
+                    <p>${v.title}</p>
+                    <p>${v.contributor}</p>
+                    <img class="rasm-1" src="${v.book_image}" alt="">
+                    <p class="mt-2">USD $${v.weeks_on_list}</p>
+                    <button class="bt-korzin" onclick="Add(${i})">Korzina</button>
+                    <button  onclick="Modal(${i})" type="button" class="bt-korzin" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                        Info
+                    </button>
+                </div>
+            </div>
+           `
+           $("#map-card").append(col)
+        })
+
+        
+    }
+
+
+
+    function Search(a) {
+        let yangi = data.filter(v=>{
+            return v.title.includes(a)
+        })
+        Ekran(yangi)
+    }
+
+
+
+
+    function Order(masiv) {
+        $("#row").html("")
+        masiv.map((v,i)=>{
+           let col = `
+            <div class="col-12">
+                <div class="map-card">
+                    <p>${v.title}</p>
+                    <p>I.E.Bowman</p>
+                    <img class="rasm-1" src="${v.book_image}" alt="">
+                    <p class="mt-2">USD $${v.weeks_on_list}</p>
+                </div>
+            </div>
+           `
+           $("#row").append(col)
+        })
+    }
+
+
+
+    function Add(i) {
+        korzinka.push(data[i])
+        Order(korzinka)
+    }
+
+
+
+    function Modal(i) {
+        $(".tik").html("")
+        let div = `
+        <div class="chap-10">
+        <img src="${data[i].book_image}" alt="" class="modal-img">
+    </div>
+    <div class="ong-10">
+        <p class="text-center">
+            ${data[i].title}
+        </p>
+        <p class="text-center text-warning"><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i></p>
+        <p class="text-center">${data[i].description}</p>
+    </div>
+        `
+        $(".tik").append(div)
+    }
